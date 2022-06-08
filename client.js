184c5f319509aebbaaca49a9a5772ca19d96a92d
VenoMidas <schuma1022@gmail.com>
2022-06-07 22:16:10 -0500

    Complete day 1 lecture practice

---
+++ client.js
@@ -0,0 +1,15 @@
+//code comments 
+console.log('Hello World!');
+
+//create a variable that stores the current year
+
+let currentYear = 2022; //this is a number, lets check!
+
+if (isNaN(currentYear)) {
+    console.log(currentYear + " is not a number");
+}else{
+    console.log(currentYear + " is a number!");
+};
+
+console.log("The current year is", currentYear);
+//looks like we were right about the number thing!
